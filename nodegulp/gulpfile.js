b2cac01c2f9b43f3c49272806fb3d596b4c0dfd4
arvind-naidu <techmaxed.net@gmail.com>
2014-01-08 21:30:36 +0800

    Initial update

--- nodegulp/gulpfile.js
+++ nodegulp/gulpfile.js
@@ -3,28 +3,28 @@ var gulp = require('gulp');
 
 // Include our plugins
 var jshint = require('gulp-jshint');
-// var sass = require('gulp-sass');
+var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 // Lint Task
 gulp.task('lint', function() {
-	gulp.src('./js/*.js')
+	gulp.src(['./js/*.js', './js/lib/*.js'])
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
 
 // Compile sass
-/* gulp.task('sass', function() {
-	gulp.src('./css/*.scss')
+gulp.task('sass', function() {
+	gulp.src('./scss/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('./css/build'));
-}); */
+		.pipe(gulp.dest('./css'));
+});
 
 // Concatenate & Minify js
 gulp.task('scripts', function() {
-	gulp.src('./js/*.js')
+	gulp.src(['./js/*.js', './js/lib/*.js'])
 		.pipe(concat('all.js'))
 		.pipe(gulp.dest('./js/build'))
 		.pipe(rename('all.min.js'))
@@ -37,11 +37,11 @@ gulp.task('default', function() {
 	gulp.run('lint', 'scripts');
 
 	// Watch for changes to our js
-	gulp.watch('./js/*.js', function() {
+	gulp.watch(['./js/*.js', './js/lib/*.js'], function() {
 		gulp.run('lint', 'scripts')
 	});
 
-	/* gulp.watch('./css/*.scss', function() {
+	gulp.watch('./scss/*.scss', function() {
 		gulp.run('sass');
-	}); */
+	});
 });
