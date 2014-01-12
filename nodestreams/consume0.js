fc2072f76b8ec2f1664554d9994e6612067efe0d
arvind-naidu <techmaxed.net@gmail.com>
2014-01-12 23:20:07 +0800

    Removed files

--- nodestreams/consume0.js
+++
@@ -1,8 +0,0 @@
-process.stdin.on('readable', function() {
-	var buf = process.stdin.read();
-	console.dir(buf);
-});
-
-/* process flow
-	- when data is available, `readable` event fires and you can call `.read()` to fetch data from buffer
-	- when the stream is finished, `.read() returns `null` because there are no more bytes to fetch
